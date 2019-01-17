c10f11c6b6fe4ff8d043c4c74aaa6d50915b1a76
Thiago Alvarez <thiago.alvarez2012@gmail.com>
2019-01-17 12:40:54 -0200

    Other entities and second migration

---
+++ SalesWebMVC/Models/Enums/SalesStatus.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SalesWebMVC.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Pending = 0, //Pendente
+        Billed = 1, //Fatorado
+        Canceled = 2 //Cancelado
+    }
+}
---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Deparment { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department deparment)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Deparment = deparment;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final) //total de vendas
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount); //a partir da lista, filtro a lista (where) de vendas contendo apenas as vendas no intervalo de datas (sr (sales record) tal que sr. Date seja maior ou igual a data inciial ou sr . date seja menor ou igual a data final) depois mando calcular a soma de (sr. que leva em sr.Amount(eu quero a soma de vendas).
+        }
+    }
+}
