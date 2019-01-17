d019fc3f9b53b33e5e1f002c256942e43ea333e5
Thiago Alvarez <thiago.alvarez2012@gmail.com>
2019-01-17 14:57:18 -0200

    SellersController

---
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
